1ebe1b290ad6b02ec82589b80ff35df846ce2df2
Goksen <goksengorgulu@gmail.com>
2020-01-22 16:30:13 +0100

    Added footer with the Github repo URL

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>
+        How is the weather today?
+      </h1>
+      <footer>
+        <a href="https://github.com/GoksenCodes/weather-react" target="_blank">Open-source code</a>
+        {" "} by Goksen Gorgulu
+      </footer>
     </div>
   );
 }
 
-export default App;
+
