bbec150d822a426dc9ee95257b6e3d4287c3feeb
Goksen <goksengorgulu@gmail.com>
2020-01-28 11:50:04 +0100

    Search engine is added

---
+++ src/Searchresult.js
@@ -0,0 +1,31 @@
+import React from "react";
+import DateComponent from "./DateComponent" ;
+
+
+export default function Searchresult(props) {
+
+    return(
+        <div className="searchresult-section">
+                <div className="row">
+                    <div className="col searchresult-left">
+                <ul>
+                    <li>
+                        <strong>{props.data.city}</strong>
+                    </li>
+                    <li><small><DateComponent date={props.data.date}/></small></li>
+                </ul>
+                </div>
+                <div className="col">
+                <ul>
+                    <li>
+                        <img src={props.data.icon} alt={props.data.description}/>
+                    </li>
+                    <li> 
+                    <strong>{Math.round(props.data.temperature)}</strong>
+                            <span><small>°C |°F</small></span></li>                
+                </ul>
+                </div>
+                </div>
+                </div>
+    )
+}
--- src/Weather.js
+++ src/Weather.js
@@ -1,21 +1,44 @@
 import React, { useState } from "react";
+
+import Searchresult from "./Searchresult" ;
 import axios from "axios" ;
 import "./Weather.css";
-import DateComponent from "./DateComponent" ;
+
 
 export default function Weather(props){
     const [weatherData , setWeatherData] = useState({ ready: false });
+    const [city , setCity] = useState(props.defaultCity) ; 
     function handleResponse(response) {
         setWeatherData({
             ready: true ,
             temperature: response.data.main.temp,
             city: response.data.name,
-            icon:"https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+            icon:`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
             description: response.data.weather[0].description,
             date: new Date(response.data.dt * 1000)
 
         })
     
+    }
+
+    function search() {
+        const apiKey = "683dab27e61c352cc6f11bcf41592476"
+        // let city = "New York"
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(handleResponse);
+    }
+    
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        search();
+
+    }
+
+    function handleInput(event) {
+        setCity(event.target.value);
+
+
     }
 
 
@@ -27,34 +50,14 @@ export default function Weather(props){
             How is the weather today?
           </h1>
                 <div className="search-section">
-                <form>
-                    <input type="search" placeholder="Enter a city" className="form-control search-box" autoFocus="on"/>
+                <form onSubmit={handleSubmit}> 
+                    <input type="search" placeholder="Enter a city" className="form-control search-box" autoFocus="on" onChange={handleInput}/>
                     <input type="submit" value="Search" className="button"/>
     
                 </form>
                 </div>
-                <div className="searchresult-section">
-                <div className="row">
-                    <div className="col searchresult-left">
-                <ul>
-                    <li>
-                        <strong>{weatherData.city}</strong>
-                    </li>
-                    <li><small><DateComponent date={weatherData.date}/></small></li>
-                </ul>
-                </div>
-                <div className="col">
-                <ul>
-                    <li>
-                        <img src={weatherData.icon} alt={weatherData.description}/>
-                    </li>
-                    <li> 
-                    <strong>{Math.round(weatherData.temperature)}</strong>
-                            <span><small>°C |°F</small></span></li>                
-                </ul>
-                </div>
-                </div>
-                </div>
+                <Searchresult data={weatherData}/>
+                
             </div>
     
     
@@ -62,12 +65,8 @@ export default function Weather(props){
 
 
     } else {
-    const apiKey = "683dab27e61c352cc6f11bcf41592476"
-    let city = "New York"
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-
-    return "Loading..."
+        search();
+        return "Loading..."
 
 
 
