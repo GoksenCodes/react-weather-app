ca9eb2a1a9ef72867ca937442afaae74d8c79669
Goksen <goksengorgulu@gmail.com>
2020-05-28 00:05:03 +0200

    fix font size & canvas size

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="#3282b8"
-      size={72}
+      size={56}
       animate={true}
     />
   );
